a1bee9aa701945dc7cb186f73676d9702cec9da7
Andrei Cazacu <andrei.r.cazacu@gmail.com>
2021-04-03 22:59:59 +0300

    logger improvements

--- src/logger.js
+++ src/logger.js
@@ -4,11 +4,33 @@ const fs = require("fs");
 const path = require("path");
 
 const specialTags = ["FOUND_PLACE", "ERROR", "SUCCESS"];
+const BYTES_PER_MB = 1024 ** 2;
 
-const logDirectory = __dirname.substr(0, __dirname.lastIndexOf(path.sep));
-let logFile = fs.createWriteStream(`${logDirectory}${path.sep}debug.log`, {
-  flags: "a",
-});
+const logDirectory =
+  __dirname.substr(0, __dirname.lastIndexOf(path.sep)) +
+  `${path.sep}logs${path.sep}`;
+
+let logFile = null;
+
+function checkLogFile() {
+  const files = fs.readdirSync(logDirectory);
+  const lastFile = files[files.length - 1];
+  const lastFileSize = fs.statSync(lastFile).size;
+
+  if (lastFileSize > 20 * BYTES_PER_MB) {
+    const sequenceNumber = lastFile.substring(
+      "debug".length,
+      lastFile.indexOf(".log")
+    );
+
+    logFile = fs.createWriteStream(
+      `${logDirectory}debug${parseInt(sequenceNumber) + 1}.log`
+    );
+  }
+}
+
+checkLogFile();
+setInterval(checkLogFile, 180000);
 
 function formatLogMessage(messageType, message) {
   const date = new Date();
